5322eb792cab736a6afc35783d92cef1404749f5
James Annal <jamesannal@gmail.com>
2016-11-30 22:20:58 +0000

    slight modification to Board and uploading

--- board.rb
+++ board.rb
@@ -4,7 +4,7 @@ class Board
    
 
   def initialize
-    @squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    @squares = (1..16).to_a
     @snakes = 14
     @ladder = 2
     @end = 16
